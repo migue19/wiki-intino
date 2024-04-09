788a00d73db34fd3ccca58fc06a1fb843c1f14b5
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2024-04-08 21:18:39 -0600

    add deployment branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'migue19', // Usually your GitHub org/user name.
   projectName: 'wiki-intino', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
